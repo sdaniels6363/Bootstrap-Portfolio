a7c0a624b684515521a7de2410ecc22357a102bc
Scott Daniels <sdaniels6363@gmail.com>
2019-09-18 17:07:42 -0400

    Finally got the sections to slide up and down thanks to jquery.

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,34 +24,68 @@ var inactiveHeadingStyle = {
 }
 
 
+
 $(document).ready(function () {
   console.log("ready!");
 
+
   $(".section-link").on("click", function () {
     // modify css of heading
-    var self = $(this).attr("id");
-    var section = self.replace("nav-", "#section-")
+    self = $(this).attr("id");
+    newSection = self.replace("nav-", "#section-")
 
-    switch (section) { // depending on which heading is clicked, we make that heading visible.
-      case "#section-1":
-        $("#section-1").css("display", "block");
-        $("#section-2").css("display", "none");
-        $("#section-3").css("display", "none");
-        break;
-      case "#section-2":
-        $("#section-1").css("display", "none");
-        $("#section-2").css("display", "block");
-        $("#section-3").css("display", "none");
-        break;
-      case "#section-3":
-        $("#section-1").css("display", "none");
-        $("#section-2").css("display", "none");
-        $("#section-3").css("display", "block");
-        break;
+    var sections = $(".section");
+    for (i=0; i<sections.length; i++){
+      if ($(sections[i]).attr("data-slide-active") === "yes"){
+        var sectionID = `#${sections[i].id}`
+        $(sectionID).attr("data-slide-active","no");
+        $(sectionID).slideUp(500);
+
+      }
     }
+    setTimeout(function(){
+      $(newSection).attr("data-slide-active","yes");
+      $(newSection).slideDown(500);
+  
+    },500);
 
-  });
-
-
-
+  }) 
 });
+    // switch (section) { // depending on which heading is clicked, we make that heading visible.
+    //   case "#section-1":
+    //       $("#section-1").slideDown(500);
+    //       $("#section-2").slideUp(500);
+    //       $("#section-3").slideUp(500);
+  
+    //     // $("#section-1").css("display", "block");
+    //     // $("#section-2").css("display", "none");
+    //     // $("#section-3").css("display", "none");
+    //     break;
+    //   case "#section-2":
+     
+
+
+    //     $("#section-1").slideUp(500);
+    //     $("#section-2").slideDown(500);
+    //     $("#section-3").slideUp(500);
+
+    //     // $("#section-1").css("display", "none");
+    //     // $("#section-2").css("display", "block");
+    //     // $("#section-3").css("display", "none");
+    //     break;
+    //   case "#section-3":
+    //       $("#section-1").slideUp(500);
+    //       $("#section-2").slideDown(500);
+    //       $("#section-3").slideDown(500);
+  
+    //     // $("#section-1").css("display", "none");
+    //     // $("#section-2").css("display", "none");
+    //     // $("#section-3").css("display", "block");
+    //     break;
+    // }
+
+//   });
+
+
+
+// });
