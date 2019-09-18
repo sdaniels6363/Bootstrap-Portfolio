318c34ec95b9fb21541afcda8c3054e9cfd263ed
Scott Daniels <sdaniels6363@gmail.com>
2019-09-17 20:11:05 -0400

    simplified the click event, and used a switch statement to display the sections

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,31 @@
 
 
-$( document ).ready(function() {
-  console.log( "ready!" );
+$(document).ready(function () {
+  console.log("ready!");
 
-  $(".slide-link").on("click", function(){
+  $(".slide-link").on("click", function () {
     // modify css of heading
     var self = $(this).attr("id");
-    var section = self.replace("nav-","#section-")
-    console.log(self);
-    console.log(section)
-    var currIndex = $(section).css("z-index");
-    console.log(currIndex);
-    $(section).css("z-index",parseInt(currIndex) + 1); // increment by one each time it's clicked
+    var section = self.replace("nav-", "#section-")
+
+    switch (section) { // depending on which heading is clicked, we make that heading visible.
+      case "#section-1":
+        $("#section-1").css("display", "block");
+        $("#section-2").css("display", "none");
+        $("#section-3").css("display", "none");
+        break;
+      case "#section-2":
+        $("#section-1").css("display", "none");
+        $("#section-2").css("display", "block");
+        $("#section-3").css("display", "none");
+        break;
+      case "#section-3":
+        $("#section-1").css("display", "none");
+        $("#section-2").css("display", "none");
+        $("#section-3").css("display", "block");
+        break;
+    }
+
   });
 
 
